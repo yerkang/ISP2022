f6f49731c809c554282249b3292266052f2a8e1c
michelle_feng <michellemfeng@gmail.com>
2022-04-26 09:15:07 -0500

    no

--- Sources/ScenesShell/Score.swift
+++ Sources/ScenesShell/Score.swift
@@ -3,17 +3,20 @@ import Scenes
 
 class Score: RenderableEntity {
     var rectangle: Rectangle
-
-    init(rect:Rect) {
+    var scoreBoard: Rectangle
+    init() {
     //    rectangle = Rectangle(rect:rect, fillMode:.fillAndStroke)
-
-        super.init(name:"Score")
+//        let rectangle = Rect(topLeft:Point(x:100, y:50), size:Size(width:200, height:300))
+  //      scoreBoard = Rectangle(rect:rectangle, fillMode:.fillAndStroke)       
+        super.init()
     }
     
     override func render(canvas:Canvas) {
-        let scoreBoard = Rect(topLeft:Point(x:100, y:50), size:Size(width:200, height:300))
-
-        let strokeStyle = StrokeStyle(color:Color(.orange))
-        let strokeStyle = StrokeStyle(color:Color(.red))
-        let lineWidth = LineWidth(width:5)
-}                                 
+        let rect = Rect(topLeft:Point(x:100, y:50), size:Size(width:200, height:300))
+        let scoreBoard = Rectangle(rect:rect, fillMode:.stroke)
+        let scorestrokeStyle = StrokeStyle(color:Color(.orange))
+        let scorefillStyle = FillStyle(color:Color(.red))
+        let scorelineWidth = LineWidth(width:5)
+        canvas.render(scorestrokeStyle, scorefillStyle, scorelineWidth, scoreBoard)
+    }
+}
