9615a8a110181af600e98dfa16e64797c72c5bc2
Yerin Kang <yerin.kang@student.allenisd.org>
2022-05-01 22:44:55 -0500

    yes

--- Sources/ScenesShell/Board.swift
+++ Sources/ScenesShell/Board.swift
@@ -2,7 +2,7 @@ import Igis
 import Scenes
 import Foundation
 
-class Board: RenderableEntity {
+class Board: RenderableEntity, KeyDownHandler{
     
     var height = 0
     
@@ -24,11 +24,11 @@ class Board: RenderableEntity {
     override func render(canvas:Canvas) {
         
             let canvasSize = canvas.canvasSize!   
-            let boardColor = FillStyle(color:Color(.black))
-            let board = Path()
+            let boardColor = StrokeStyle(color:Color(.black))
+            let board = Path(fillMode: .stroke)
             // x value should be half of the building's width    
-            board.moveTo(Point(x:10, y:(canvasSize.height/3)*2))
-            board.lineTo(Point(x:10, y:((canvasSize.height/3)*2) + height))
+            board.moveTo(Point(x:10, y:(canvasSize.height/4)*3))
+            board.lineTo(Point(x:10, y:((canvasSize.height/4)*3) + height))
             // y value should be canvasSize - buildingHeight + height increments
             let boardWidth = LineWidth(width: 5)
             canvas.render(boardColor, boardWidth, board)
--- Sources/ScenesShell/InteractionLayer.swift
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -1,4 +1,5 @@
 import Scenes
+import Igis
 
   /*
      This class is responsible for the interaction Layer.
@@ -6,13 +7,19 @@ import Scenes
    */
 
 
-class InteractionLayer : Layer {
+class InteractionLayer : Layer, KeyDownHandler {
 
       init() {
           // Using a meaningful name can be helpful for debugging
           super.init(name:"Interaction")
 
           // We insert our RenderableEntities in the constructor
-
+          insert(entity: Board, at: .front)
+      }
+      override func presetup(canvasSize:size, canvas:Canvas){
+          dispatcher.registerKeyDownHandler(handler: self)
+      }
+      override func postTeardown() {
+          dispatcher.unregisterKeyDownHandler(handler: self)
       }
   }
