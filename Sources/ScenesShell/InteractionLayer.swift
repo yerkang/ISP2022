7987d6f9753ae6b5e1b7405546bf4be3b5d379e2
Shivanshi Das <shivanshi.das@student.allenisd.org>
2022-05-06 18:35:45 -0500

    Bug

--- Sources/ScenesShell/InteractionLayer.swift
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -12,8 +12,11 @@ import Foundation
 class InteractionLayer : Layer, KeyDownHandler {
 
 
-    let board = Board(rect: Rect(size:Size(width:10, height:100)))
+    let board = Board(rect: Rect(size:Size(width:10, height:-10)))
     let background = Background()
+    let wides = [50, 35, 10, -5, 10, 20, -10, 30, -10, 15, 20, -15]
+
+ 
 
 
       init() {
@@ -27,15 +30,27 @@ class InteractionLayer : Layer, KeyDownHandler {
       }
       
       func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool) {
-         
+var x = 0         
           if key  == "x" {
-              board.extend(height: 50)
+              board.extend(height: 10)
+          }
+
+          if key == "f" {
+              board.fall()
               
-        }}
+              board.calculate(canvasSize: Size, canvas: Canvas, widthBetween: adds, widthOf: wides, n: x)
+          x += 1}
+      }
 
     override func preSetup(canvasSize: Size, canvas: Canvas) {
         dispatcher.registerKeyDownHandler(handler: self)
-        board.move(to: Point(x:10, y:((canvasSize.height/3)*2)))
+        let canvasSize = canvas.canvasSize!
+        let width = canvasSize.width
+        let height = canvasSize.height
+ 
+        var h = 0
+        var v = 0
+        board.move(to: Point(x:(adds[h] + wides[v]/2), y:300 + height/6 - 10))
         
     }
 
